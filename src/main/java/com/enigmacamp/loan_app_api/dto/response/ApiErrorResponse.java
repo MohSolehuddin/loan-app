0845c25b779b6af8e7a27218abf158b4e3a5c7ea
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:12:05 +0700

    FEAT: Create API error response

--- src/main/java/com/enigmacamp/loan_app_api/dto/response/ApiErrorResponse.java
+++ src/main/java/com/enigmacamp/loan_app_api/dto/response/ApiErrorResponse.java
@@ -1,2 +1,13 @@
-package com.enigmacamp.loan_app_api.dto.response;public class ApiErrorResponse {
+package com.enigmacamp.loan_app_api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiErrorResponse {
+    private int statusCode;
+    private String message;
 }
