825eb83ce4ddc5cf33834a477b7e4773c8e81971
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:11:34 +0700

    FEAT: Create register request dto

--- src/main/java/com/enigmacamp/loan_app_api/dto/request/RegisterRequest.java
+++ src/main/java/com/enigmacamp/loan_app_api/dto/request/RegisterRequest.java
@@ -1,4 +1,28 @@
 package com.enigmacamp.loan_app_api.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "email is required")
+    @Email
+    private String email;
+    @NotBlank(message = "password is required")
+    private String password;
+    @NotBlank(message = "First name is required")
+    private String firstName;
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+    private String dateOfBirth;
+    private String phone;
+    private String status;
 }
