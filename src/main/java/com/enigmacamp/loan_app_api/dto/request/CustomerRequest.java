bf6ade3ce0e73b1e68552da08578a7f5cc7ed99f
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:10:17 +0700

    FEAT: fix

--- src/main/java/com/enigmacamp/loan_app_api/dto/request/CustomerRequest.java
+++ src/main/java/com/enigmacamp/loan_app_api/dto/request/CustomerRequest.java
@@ -1,9 +1,12 @@
 package com.enigmacamp.loan_app_api.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Date;
 
 @AllArgsConstructor
 @Builder
@@ -11,9 +14,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerRequest {
         private String id;
+        @NotBlank(message = "First name is required")
         private String firstName;
+        @NotBlank(message = "Last name is required")
         private String lastName;
-        private String dateOfBirth;
+        private Date dateOfBirth;
         private String phone;
         private String status;
 }
