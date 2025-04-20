d71c8b11ff67cbb7dd2d08470aee675d23aa082b
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:11:10 +0700

    FEAT: Create loan transaction pay dto

--- src/main/java/com/enigmacamp/loan_app_api/dto/request/LoanTransactionPayRequest.java
+++ src/main/java/com/enigmacamp/loan_app_api/dto/request/LoanTransactionPayRequest.java
@@ -1,2 +1,14 @@
-package com.enigmacamp.loan_app_api.dto.request;public class LoanTransactionPayRequest {
+package com.enigmacamp.loan_app_api.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoanTransactionPayRequest {
+    private Double nominal;
 }
