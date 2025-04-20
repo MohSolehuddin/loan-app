c719dfafeb52550d630d5ad35e179a4c928540fe
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:14:43 +0700

    FIX: list loan transaction detail

--- src/main/java/com/enigmacamp/loan_app_api/mapper/LoanTransactionMapper.java
+++ src/main/java/com/enigmacamp/loan_app_api/mapper/LoanTransactionMapper.java
@@ -5,21 +5,36 @@ import com.enigmacamp.loan_app_api.dto.response.LoanTransactionResponse;
 import com.enigmacamp.loan_app_api.entity.LoanTransaction;
 import com.enigmacamp.loan_app_api.entity.LoanTransactionDetail;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class LoanTransactionMapper {
-    public static LoanTransactionResponse mapToResponse(LoanTransaction transaction){
-        LoanTransactionDetailResponse loanTransactionDetailResponse = LoanTransactionDetailMapper.mapToResponse((LoanTransactionDetail) transaction.getLoanTransactionDetails());
+    public static LoanTransactionResponse mapToResponse(LoanTransaction transaction) {
+        List<LoanTransactionDetailResponse> loanTransactionDetailResponses =
+                transaction.getLoanTransactionDetails() != null
+                        ? transaction.getLoanTransactionDetails().stream()
+                        .map(LoanTransactionDetailMapper::mapToResponse)
+                        .toList()
+                        : List.of();
+
+
         return LoanTransactionResponse.builder()
                 .id(transaction.getId())
                 .nominal(transaction.getNominal())
-                .instalmentTypeId(transaction.getInstalmentType().getId())
-                .loanTypeId(transaction.getLoanType().getId())
+                .instalmentTypeId(
+                        transaction.getInstalmentType() != null ? transaction.getInstalmentType().getId() : null)
+                .loanTypeId(
+                        transaction.getLoanType() != null ? transaction.getLoanType().getId() : null)
                 .createdAt(transaction.getCreatedAt())
                 .updateAt(transaction.getUpdatedAt())
-                .approvedStatus(transaction.getApprovalStatus().toString())
+                .approvedStatus(
+                        transaction.getApprovalStatus() != null ? transaction.getApprovalStatus().toString() : null)
                 .approvedAt(transaction.getApprovedAt())
                 .approvedBy(transaction.getApprovedBy())
-                .customerId(transaction.getCustomer().getId())
-                .transactionDetailResponses(loanTransactionDetailResponse)
+                .customerId(
+                        transaction.getCustomer() != null ? transaction.getCustomer().getId() : null)
+                .transactionDetailResponses(loanTransactionDetailResponses)
                 .build();
     }
 }
