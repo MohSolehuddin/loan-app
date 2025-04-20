5e3b4b7014bca67703560d4be9161144babd67f7
Moh Solehuddin <moh.solehuddin190805@gmail.com>
2025-04-20 15:13:48 +0700

    FEAT: create global exaction handler

--- src/main/java/com/enigmacamp/loan_app_api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/enigmacamp/loan_app_api/exception/GlobalExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.enigmacamp.loan_app_api.exception;
 
+import com.enigmacamp.loan_app_api.dto.response.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.dao.DataIntegrityViolationException;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<?> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        String errorMessage = "Data conflict error";
+        if (ex.getMostSpecificCause().getMessage().contains("duplicate key value violates unique constraint")) {
+            errorMessage = "Email already exists.";
+        }
+        return new ResponseEntity<>(new ApiErrorResponse(HttpStatus.CONFLICT.value(), errorMessage), HttpStatus.CONFLICT);
+    }
 }
